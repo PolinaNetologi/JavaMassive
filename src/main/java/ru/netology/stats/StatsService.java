7b491e841cb62ff671c99e55b1c6e3a8f297fcb2
Polina <pzmujdeckaa@gmail.com>
2023-04-17 17:25:25 +0500

    JavaMassive

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,13 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public long sum(long[] sales) {
+        long totalSale = 0;
+
+        for (long Sale : sales) {
+            totalSale += 0;
+        }
+        return totalSale;
+    }
+
+}
