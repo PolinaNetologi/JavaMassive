3de3a6d51abde794cb2f93a4a903acd6e2c36eee
Polina <pzmujdeckaa@gmail.com>
2023-04-18 21:41:00 +0500

    JavaMassive

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,13 +1,64 @@
 package ru.netology.stats;
 
 public class StatsService {
-    public long sum(long[] sales) {
-        long totalSale = 0;
+    public int sum(int[] sales) {
+        int sumSale = 0;
 
-        for (long Sale : sales) {
-            totalSale += 0;
+        for (int i = 0; i < sales.length; i++) {
+            sumSale = sumSale + sales[i];
         }
-        return totalSale;
+        return sumSale;
     }
 
+    public int average(int[] sales) {
+        return sum(sales) / sales.length;
+    }
+
+    public int maxMonthSale(int[] sales) {
+        int maxMoth = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMoth]) {
+                maxMoth = i;
+            }
+        }
+        return maxMoth + 1;
+    }
+
+    public int minMonthSale(int[] sales) {
+        int minMoth = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMoth]) {
+                minMoth = i;
+            }
+        }
+        return minMoth + 1;
+    }
+
+    public int mothBellowAverage(int[] sales) {
+        int counter = 0;
+        for (int sale : sales) {
+            if (sale < average(sales)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public int mothNellAverage(int[] sales) {
+        int counter = 0;
+        for (int sale : sales) {
+            if (sale > average(sales)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
 }
+
+
+
+
+
+
+
+
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,5 +1,4 @@
 package ru.netology.stats;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -7,12 +6,62 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StatsServiceTest {
     @Test
-    public void testSum() {
+    public void testSumSale() {
         StatsService service = new StatsService();
-        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 18, 14};
 
-        long actual = service.sum(sales);
-        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
-        Assertions.assertEquals(expected, actual);
+        int actual = service.sum(sales);
+        int expectedSum = 180;
+        Assertions.assertEquals(expectedSum, actual);
+    }
+
+    @Test
+    public void testAverage() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 18, 14};
+
+        int actual = service.average(sales);
+        int expectedSum = 180 / 12;
+        Assertions.assertEquals(expectedSum, actual);
+    }
+
+    @Test
+    public void testMaxMoth() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 18, 14};
+
+        int actual = service.maxMonthSale(sales);
+        int expectedSum = 8;
+        Assertions.assertEquals(expectedSum, actual);
+    }
+
+    @Test
+    public void testMinMoth() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 18, 14};
+
+        int actual = service.minMonthSale(sales);
+        int expectedSum = 9;
+        Assertions.assertEquals(expectedSum, actual);
+    }
+
+    @Test
+    public void testMothBellowAverage() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 18, 14};
+
+        int actual = service.mothBellowAverage(sales);
+        int expectedSum = 5;
+        Assertions.assertEquals(expectedSum, actual);
+    }
+
+    public void testMothNellAverage() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 18, 14};
+
+        int actual = service.mothNellAverage(sales);
+        int expectedSum = 7;
+        Assertions.assertEquals(expectedSum, actual);
     }
 }
+
